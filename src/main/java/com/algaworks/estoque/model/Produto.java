6b09b0ff78913aac3814b8bd7592494f872a72b2
renatoguara2020 <renatoguara2020@gmail.com>
2023-10-10 07:51:28 -0300

    ok

--- src/main/java/com/algaworks/estoque/model/Produto.java
+++ src/main/java/com/algaworks/estoque/model/Produto.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -15,7 +16,7 @@ public class Produto implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String nome;
